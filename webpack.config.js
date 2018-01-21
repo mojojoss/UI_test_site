8150b04ca338c3a18092a46790603c8c6f2c64ed
Mojo <mojojoss1@gmail.com>
2018-01-21 20:45:01 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	context: path.resolve(__dirname, './'),
+	entry: {
+		index: './main.js'
+	},
+	output: {
+		path: path.resolve(__dirname, './build'),
+		filename: '[name].bundle.js',
+		publicPath: ''
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, './')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader'
+					}
+				]
+			},
+
+			{
+				test: /\.js$/,
+				exclude: [/node_modules/],
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['es2015']
+						}
+                    }
+                ]
+            },
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader'
+				})
+            },
+
+			{
+				test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+				use: 'file-loader'
+			},
+			{
+				test: /\.pug$/,
+				use:['raw-loader', 'pug-html-loader'],
+			},
+
+
+
+
+			]
+
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jquery: 'jquery'
+		}),
+		new HtmlWebpackPlugin({
+            filename:'index.html',
+			template:'index.pug'
+		}),
+        new HtmlWebpackPlugin({
+            filename:'news.html',
+			template:'news.pug'
+		}),
+        new HtmlWebpackPlugin({
+            filename:'messages.html',
+			template:'messages.pug'
+		}),
+        new HtmlWebpackPlugin({
+            filename:'feedback.html',
+			template:'feedback.pug'
+		}),
+		new ExtractTextPlugin('styles.css')
+	]
+};
